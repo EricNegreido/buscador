f096c1ed6650d9548466a4299d80fa2231a27f31
Eric Negreido <ericnegreidooo@gmail.com>
2024-04-14 11:43:14 -0300

    se modifico la forma de consultas y se utilza consultas preparas

--- pagina_busqueda.php
+++ pagina_busqueda.php
@@ -14,8 +14,8 @@
 
   $conexion = mysqli_connect($db_host, $db_usuario, $db_contra);
   
-  // con mysqli_real_escape_string evitamos la inyeccion de datos, esta funcion no permite caracteres indebidos
-  $mibusqueda = mysqli_real_escape_string($conexion, $data);
+// con mysqli_real_escape_string evitamos la inyeccion de datos, esta funcion no permite caracteres indebidos
+  // $mibusqueda = mysqli_real_escape_string($conexion, $data);
 
   if(mysqli_connect_errno()){
     echo "Fallo al conectar con la BBDD";
@@ -24,7 +24,32 @@
 
   mysqli_select_db($conexion, $db_nombre) or die("No se encuetra la BDD");
   mysqli_set_charset($conexion, "utf8");
-  $consulta = "SELECT * FROM persons WHERE name LIKE'%$mibusqueda%'";
+
+// CONSULTA PREPARADA
+
+// 1°_creamos sentencia sql
+  $sql = "SELECT name, last_name, dni FROM persons WHERE name= ?";
+// 2°_Preparo consulta
+  $result = mysqli_prepare($conexion, $sql);
+// 3°_Unir parametros
+  $success = mysqli_stmt_bind_param($result, "s", $data);
+// 4°_Ejecutar sql
+  $execute = mysqli_stmt_execute($result);
+
+  if($execute == false){
+    echo "Error al ejecutar la consulta";
+  }else{
+// 5°_Ascociar variables
+   $ok = mysqli_stmt_bind_result($result, $name, $last_name, $dni);
+   echo "Personas encontradas: <br>";
+   echo "nombre" . " " . "apellido" . " " . "dni" . "<br>";
+   while(mysqli_stmt_fetch($result)){
+    echo $name . " " . $last_name . " " . $dni . "<br>";
+   }
+   mysqli_stmt_close($result);
+  }
+// CONSULTA NORMAL
+  // $consulta = "SELECT * FROM persons WHERE name LIKE'%$mibusqueda%'";
 
   $resultado = mysqli_query($conexion, $consulta);
   
